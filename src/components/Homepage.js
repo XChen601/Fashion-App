295ffb5dd74e1e34c1a7ce6ffdd38905a1fbe6c4
xchen <xchen601@gmail.com>
2023-08-23 15:39:09 -0400

    clothings and laundry functionalities

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Homepage() {
+  function JavascriptFunc() {
+    alert("Hello World!");
+  }
+
   return (
     <div>
       <h1>Homepage</h1>
       <div>SOMETHING</div>
+      <div>HTML can just be written here</div>
     </div>
   );
 }
