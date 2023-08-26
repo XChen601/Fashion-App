082f96e380e2cbd4d3366db7f9fa3c2d59a5eeeb
Bryan Rivera <bryanrivera21403@gmail.com>
2023-08-25 21:00:14 -0400

    added weather api page

--- src/components/WeatherPanel.js
+++ src/components/WeatherPanel.js
@@ -3,30 +3,63 @@ import "../css/WeatherPanel.css";
 import Navigation from "./Navigation";
 
 function WeatherPanel({ weatherJSON, zipCode }) {
-  console.log("");
+  console.log(zipCode);
   return (
-    <div>
-      <div>Zip Code: {zipCode}</div>
+    <div id="weather-panel">
+      <div id="weather-container-1">
+        {/* <h2 id="zip-code">Zip Code: {zipCode}</h2> */}
+        <h2 id="place">{weatherJSON.name}</h2>
 
-      <div>{weatherJSON.weather[0].description.toUpperCase()}</div>
+        <h1 id="description">{weatherJSON.weather[0].description.toUpperCase()}</h1>
 
-      <img
-        src={`https://openweathermap.org/img/wn/${weatherJSON.weather[0].icon}@4x.png`}
-      />
+        <img id="icon"
+          src={`https://openweathermap.org/img/wn/${weatherJSON.weather[0].icon}@4x.png`}
+        />
+      </div>
+      <div id="weather-container-2">
+        <div>
+          <h2>Temperature</h2>
+          <h3 id="temperature"> {Math.round(weatherJSON.main.temp)}°F</h3>
+        </div>
 
-      <div>{Math.round(weatherJSON.main.temp)}°F</div>
+        <div>
+          <h2>Low</h2>
+          <h3 id="low-temp">{Math.round(weatherJSON.main.temp_min)}°F</h3>
+        </div>
 
-      <div>{Math.round(weatherJSON.main.temp_min)}°F</div>
+        <div>
+          <h2>High</h2>
+          <h3 id="high-temp">{Math.round(weatherJSON.main.temp_max)}°F</h3>
+        </div>
 
-      <div>{Math.round(weatherJSON.main.temp_max)}°F</div>
+        <div>
+          <h2>Feels like</h2>
+          <h3 id="feels-like">
+            {Math.round(weatherJSON.main.feels_like)}°F
+          </h3>
+        </div>
+      </div>
+      <div id="weather-container-3">
+        <div>
+          <h2>Wind Speed</h2>
+          <h3 id="wind-speed">
+            {weatherJSON.wind.speed} mph
+          </h3>
+        </div>
+        <div>
+          <h2>Wind Pressure</h2>
+          <h3 id="wind-pressure">
+            {weatherJSON.main.pressure} mb
+          </h3>
+        </div>
+        <div>
+          <h2>Humidity</h2>
+          <h3 id="humidity">
+            {weatherJSON.main.humidity}%
+          </h3>
+        </div>
+      </div>
 
-      <div>{Math.round(weatherJSON.main.feels_like)}°F</div>
-
-      <div>{weatherJSON.wind.speed} mph</div>
-
-      <div>{weatherJSON.main.pressure} mb</div>
-
-      <div>{weatherJSON.main.humidity}%</div>
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,82 +2,68 @@ import React, { useState } from "react";
 import "../css/Welcome.css";
 import Navigation from "./Navigation";
 import WeatherPanel from "./WeatherPanel";
-import { NavbarBrand } from "reactstrap";
 
 function Welcome() {
-  const apiKey = "311762ba11c494164f939186b39a89b6";
-  let zipURL;
-  let zipValue;
-  let zipLat;
-  let zipLon;
-  let fetchedWeatherData;
+    const apiKey = "311762ba11c494164f939186b39a89b6";
+    let zipURL, zipValue, zipLat, zipLon;
+    let time = "Morning";
+    let name = "John Smith";
 
-  const [input, setInput] = useState("");
-  const [data, setData] = useState(null);
+    const [input, setInput] = useState("");
+    const [data, setData] = useState(null);
+    const [zip, setZip] = useState(null);
 
-  const handleInput = (event) => {
-    setInput(event.target.value);
-  };
+    const handleInput = (event) => {
+        setInput(event.target.value);
+    };
 
-  const handleClick = () => {
-    submitForm(input);
-  };
+    const handleClick = async () => {
+        await submitForm(input);
+    };
 
-  const submitForm = (value) => {
-    zipValue = value;
-    zipURL = `https://api.openweathermap.org/geo/1.0/zip?zip=${value},US&appid=${apiKey}`;
-    fetch(zipURL)
-      .then(function (reponse) {
-        return reponse.json();
-      })
-      .then(function (zipJSON) {
+    const submitForm = async (value) => {
+        zipValue = value;
+        zipURL = `https://api.openweathermap.org/geo/1.0/zip?zip=${value},US&appid=${apiKey}`;
+        const zipResponse = await fetch(zipURL);
+        const zipJSON = await zipResponse.json();
         zipLat = zipJSON.lat;
         zipLon = zipJSON.lon;
         if (zipLat == undefined || zipLon == undefined) {
-          alert("Invalid zip code.");
-          return;
+            alert("Invalid zip code.");
         }
         const weatherDataURL = `https://api.openweathermap.org/data/2.5/weather?lat=${zipLat}&lon=${zipLon}&units=imperial&appid=${apiKey}`;
-        fetchWeather(weatherDataURL);
-      });
-  };
+        const weatherResponse = await fetch(weatherDataURL);
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData);
+        setData(weatherData);
+        setZip(zipValue)
+    };
 
-  const fetchWeather = async (url) => {
-    try {
-      const response = await fetch(url);
-      const weatherData = await response.json();
-      console.log(weatherData);
-      setData(weatherData);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return (
+        <div className="parent-div">
+            <Navigation />
+            <div className="welcome-top">
+                <div className="welcome-time">
+                    <h1>Good {time}</h1>
+                </div>
+                <div className="welcome-user">
+                    <h1>Welcome, {name}</h1>
+                </div>
+            </div>
+            <div id="form">
+                <input id="text-box" value={input} onChange={handleInput} placeholder="Enter Zip Code" />
 
-  return (
-    <div className="parent-div">
-      <Navigation />
-      <div className="welcome-top">
-        <div className="welcome-time">
-          <h1>Good Morning</h1>
+                <button id="click-button" onClick={handleClick}>Submit</button>
+            </div>
+            <div>
+                {data && (
+                    <WeatherPanel
+                        weatherJSON={data}
+                        zipCode={zip} />
+                )}
+            </div>
         </div>
-        <div className="welcome-user">
-          <h1>Welcome, Bryan Rivera</h1>
-        </div>
-        <img src={require("./img/logo.png")} />
-      </div>
-      <div>
-        <input value={input} onChange={handleInput} />
-
-        <button onClick={handleClick}>Submit</button>
-      </div>
-      <div>
-        {data && (
-          <WeatherPanel weatherJSON={fetchedWeatherData} zipCode={zipValue} />
-        )}
-        <Navigation />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Welcome;
